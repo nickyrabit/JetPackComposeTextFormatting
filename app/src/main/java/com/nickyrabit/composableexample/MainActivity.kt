da5226e31740a13af18677670ed5d5227faa80b0
Nicky Ngailo <nickyrabit@gmail.com>
2019-12-02 17:06:52 +0300

    init

--- app/src/main/java/com/nickyrabit/composableexample/MainActivity.kt
+++ app/src/main/java/com/nickyrabit/composableexample/MainActivity.kt
@@ -3,10 +3,16 @@ package com.nickyrabit.composableexample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.setContent
-import androidx.ui.layout.Column
-import androidx.ui.layout.FlexColumn
+import androidx.ui.core.*
+import androidx.ui.foundation.VerticalScroller
+import androidx.ui.graphics.Color
+import androidx.ui.layout.*
 import androidx.ui.material.MaterialTheme
+import androidx.ui.text.TextStyle
+import androidx.ui.text.font.Font
+import androidx.ui.text.font.FontFamily
+import androidx.ui.text.font.FontStyle
+import androidx.ui.text.font.FontWeight
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
@@ -26,11 +32,47 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun CustomFontDisplay() {
-        FlexColumn(
+//Wrapping Padding to leave white space at the sides
+Padding(20.dp){
 
-        ) {
-            
+    // Vertical Scroller will help when the screen is small or yu have lots of data
+    VerticalScroller() {
+//Column arranges the children vertically
+        Column(mainAxisAlignment = MainAxisAlignment.Start,
+            crossAxisAlignment = CrossAxisAlignment.Stretch
+
+
+        ){
+
+            Text("I am Custom Color Blue", style = TextStyle(color = Color.Blue))
+            Text("I am Custom Font Montserrat", style = TextStyle(fontFamily = FontFamily(Font(name="montserrat_semibold.otf"))))
+            Text("I am Italic",style = TextStyle(fontStyle = FontStyle.Italic))
+            Text("I am BOLD", style = TextStyle(fontWeight = FontWeight.Bold))
+            Text("I am Large Text", style = TextStyle(fontSize = 25.sp))
+            Text("But We are all Custom  Made Fonts")
+
+
+
+
         }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
+
+
+
     
 }
 
