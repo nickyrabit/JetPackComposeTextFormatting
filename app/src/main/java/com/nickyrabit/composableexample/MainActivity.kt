6605c7567da7fa0ef6c70a35ec834183650cd807
Nicky Ngailo <nickyrabit@gmail.com>
2019-12-02 14:38:07 +0300

    adding a flex colulmn

--- app/src/main/java/com/nickyrabit/composableexample/MainActivity.kt
+++ app/src/main/java/com/nickyrabit/composableexample/MainActivity.kt
@@ -4,25 +4,40 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.setContent
+import androidx.ui.layout.Column
+import androidx.ui.layout.FlexColumn
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
+   
+ 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MaterialTheme() {
+                CustomFontDisplay()
+            }
         }
     }
 }
 
 @Composable
-fun StarbucksCofee(name: String) {
+fun CustomFontDisplay() {
+        FlexColumn(
 
-    }
+        ) {
+            
+        }
+    
+}
 
 @Preview
 @Composable
 fun DefaultPreview() {
-
+    MaterialTheme (){
+        CustomFontDisplay()
+    }
 }
